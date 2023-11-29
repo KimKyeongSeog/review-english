282566292246b7eec68025e551eaa0744999d4a2
KimKyeongSeog <kimks9241@naver.com>
2023-11-29 16:37:52 +0900

    fin

--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import englishData from "../englishData.json";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Day = () => {
   const [dailyData, setDailyData] = useState();
@@ -8,6 +9,8 @@ const Day = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   const [currentPage, setCurrentPage] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const { day } = useParams();
 
@@ -21,6 +24,55 @@ const Day = () => {
     currentPage === 0
       ? setCurrentPage(dailyData.sentences.length - 1)
       : setCurrentPage(currentPage - 1);
+  };
+
+  const onClickSound = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isLoading) return;
+
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.sentences[currentPage].english
+          },
+          voice: {
+            languageCode: "en-gb",
+            name: "en-GB-Standard-A",
+            ssmlGender: "FEMALE"
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+            speakingRate: "1",
+            pitch: "1"
+          }
+        }
+      );
+
+      console.log(response);
+
+      const binaryData = atob(response.data.audioContent);
+
+      const byteArray = new Uint8Array(binaryData.length);
+
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3" });
+
+      const newAudio = new Audio(URL.createObjectURL(blob));
+
+      document.body.appendChild(newAudio);
+      newAudio.play();
+
+      setTimeout(() => setIsLoading(false), 5000);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -47,10 +99,13 @@ const Day = () => {
       </h1>
       <div className="mt-12">
         <div className="font-['GangWon'] text-2xl">
+          {dailyData.sentences[currentPage].number}
           {dailyData.sentences[currentPage].english}
         </div>
         <button
-          className={`font-['GangWon'] text-2xl ${!isVisible && "bg-black"}`}
+          className={`text-left font-['GangWon'] text-2xl ${
+            !isVisible && "bg-black"
+          }`}
           onClick={() => setIsVisible(!isVisible)}
         >
           {dailyData.sentences[currentPage].korean}
@@ -69,7 +124,10 @@ const Day = () => {
           >
             Next
           </button>
-          <button className="btn-style py-1 px-2 bg-blue-400 text-white font-semibold  rounded-xl ml-2">
+          <button
+            className="btn-style py-1 px-2 bg-blue-400 text-white font-semibold  rounded-xl ml-2"
+            onClick={onClickSound}
+          >
             Sound
           </button>
         </div>
@@ -79,3 +137,17 @@ const Day = () => {
 };
 
 export default Day;
+
+// 1. api 가져오기
+
+// 2. binaryData = atob(response.data.audioContent); base64 형식의 데이터 디코딩
+
+// 3.  const byteArray = new Uint8Array(binaryData.length);
+
+// for (let i = 0; i < binaryData.length; i++) {
+//   byteArray[i] = binaryData.charCodeAt(i);
+// } 을 통해 바이트 단위로 쪼갠 후 숫자로 출력
+
+// 4. 바이너리 라지 오브젝트(BLOB)로 다시 변경
+// 5. html 사용 (오디오 사용, 다운로드)
+// 6. 딜레이 설정
