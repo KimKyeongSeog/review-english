1cd3dfd9f069637b5e3fb1f98e8c5dba647bcee8
KimKyeongSeog <kimks9241@naver.com>
2023-11-29 13:55:49 +0900

    almost-done(without-sound)

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -4,7 +4,7 @@ const MainCard = ({ title, day }) => {
   return (
     <Link to={`/${day}`}>
       <div className="flex p-4 gap-4 font-['Helvetica_Neu_Bold']">
-        <span className=" bg-blue-400 text-white inline-block rounded-xl w-12 h-12 text-sm text-center pt-[12px] font-semibold">
+        <span className="  inline-block bg-blue-400 text-white rounded-xl w-12 h-12 text-sm text-center pt-[12px] font-semibold">
           Day {day}
         </span>
         <span className="font-['GangWon'] pt-3 duration-150 hover:font-extrabold hover:text-3xl text-2xl hover:border-b-[1px] border-black">
--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -1,9 +1,81 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import englishData from "../englishData.json";
+import { useEffect, useState } from "react";
 
 const Day = () => {
+  const [dailyData, setDailyData] = useState();
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(0);
+
   const { day } = useParams();
 
-  return <div>Day {day}</div>;
+  const onClickNext = () => {
+    currentPage === dailyData.sentences.length - 1
+      ? setCurrentPage(0)
+      : setCurrentPage(currentPage + 1);
+  };
+
+  const onClickPrev = () => {
+    currentPage === 0
+      ? setCurrentPage(dailyData.sentences.length - 1)
+      : setCurrentPage(currentPage - 1);
+  };
+
+  useEffect(() => {
+    englishData.forEach((v) => {
+      if (v.day === +day) {
+        setDailyData(v);
+      }
+    });
+  }, [day]);
+  //map은 리턴값을 요구, forEach로!
+  useEffect(() => console.log(dailyData), [dailyData]);
+
+  if (!dailyData) return <div>Loading......</div>;
+
+  return (
+    <div className="container relative">
+      <div className="absolute">
+        <button className="btn-style py-1 px-2 bg-pink-400 text-white font-semibold  rounded-xl ml-2">
+          <Link to="/">Back</Link>
+        </button>
+      </div>
+      <h1 className="text-center text-2xl font-semibold text-blue-500">
+        Day {dailyData.day} - {dailyData.title}
+      </h1>
+      <div className="mt-12">
+        <div className="font-['GangWon'] text-2xl">
+          {dailyData.sentences[currentPage].english}
+        </div>
+        <button
+          className={`font-['GangWon'] text-2xl ${!isVisible && "bg-black"}`}
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {dailyData.sentences[currentPage].korean}
+        </button>
+
+        <div className="mt-4 ml-2">
+          <button
+            className="btn-style py-1 px-2 bg-blue-400 text-white rounded-xl font-semibold"
+            onClick={onClickPrev}
+          >
+            Prev
+          </button>
+          <button
+            className="btn-style py-1 px-2 bg-blue-400 text-white font-semibold rounded-xl ml-2"
+            onClick={onClickNext}
+          >
+            Next
+          </button>
+          <button className="btn-style py-1 px-2 bg-blue-400 text-white font-semibold  rounded-xl ml-2">
+            Sound
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Day;
